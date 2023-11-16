5c938e557706fa197e79b498e6ad89940b4bd0c2
PrathmeshU20 <prathmeshurkude20@gmail.com>
2023-11-17 02:02:56 +0530

    lab05 commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get("/", function () {
+    return "Hello from RCOEM";
+});
+
+Route::get("/aboutus", function () {
+    return view("aboutus");
+});
+
+Route::view("/contact","contact", [
+    "name" => "Prathmesh"
+]);
+
+Route::get("/person/{name}", function($name){
+    return "The name is $name.";
+});
+
+Route::get('/person/{name?}', function(?string $name = "Prathmesh Urkude (default)"){
+    return "The name is $name.";
+})->name('person');
